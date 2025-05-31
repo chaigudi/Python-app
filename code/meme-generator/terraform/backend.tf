2ce32f14a8d1b60dddcb8b680f8a50ceb28e953b
Chaithanya Gudiwada <116893885+chaigudi@users.noreply.github.com>
2025-05-31 21:03:36 +0530

    Update backend.tf

--- code/meme-generator/terraform/backend.tf
+++ code/meme-generator/terraform/backend.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
 
+  backend "s3" {
+    bucket  = "my-s3-bucket-backend-4556"
+    key     = "eks/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
