d689bed7d33ce622c4cde57e5a9423944d041b07
Chaithanya Gudiwada <116893885+chaigudi@users.noreply.github.com>
2025-06-03 17:04:06 +0530

    Create providers.tf

---
+++ code/meme-generator/terraform/providers.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "my-s3-bucket-backend-4556"
+    key     = "eks/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = var.common_tags
+  }
+}
+
+# Kubernetes Provider
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# Helm Provider
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
