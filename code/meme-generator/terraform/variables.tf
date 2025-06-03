090a0ed4c1ab7a078680ee25baf5a08e16c5c41f
Chaithanya Gudiwada <116893885+chaigudi@users.noreply.github.com>
2025-06-03 17:03:24 +0530

    Update variables.tf

--- code/meme-generator/terraform/variables.tf
+++ code/meme-generator/terraform/variables.tf
@@ -1,7 +1,124 @@
+# General Variables
 variable "region" {
-  default = "ap-south-1"
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
 
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "development"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "eks-demo"
+}
+
+variable "common_tags" {
+  description = "Common tags to be applied to all resources"
+  type        = map(string)
+  default = {
+    Environment = "development"
+    Project     = "eks-demo"
+    ManagedBy   = "terraform"
+    Owner       = "devops-team"
+  }
+}
+
+# EKS Cluster Variables
 variable "cluster_name" {
-  default = "eks-cluster-demo"
+  description = "EKS cluster name"
+  type        = string
+  default     = "eks-cluster-demo"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for EKS cluster"
+  type        = string
+  default     = "1.29"
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Enable public API server endpoint"
+  type        = bool
+  default     = true
+}
+
+variable "cluster_endpoint_private_access" {
+  description = "Enable private API server endpoint"
+  type        = bool
+  default     = true
+}
+
+# VPC Variables
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Use single NAT Gateway for all private subnets"
+  type        = bool
+  default     = true
+}
+
+# Node Groups Variables
+variable "node_groups" {
+  description = "EKS managed node groups configuration"
+  type = map(object({
+    instance_types = list(string)
+    min_size       = number
+    max_size       = number
+    desired_size   = number
+    ami_type       = string
+    capacity_type  = string
+    disk_size      = number
+  }))
+  default = {
+    default = {
+      instance_types = ["t2.micro"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      ami_type       = "AL2_x86_64"
+      capacity_type  = "ON_DEMAND"
+      disk_size      = 20
+    }
+   }
+  }
+}
+
+# ALB Controller Variables
+variable "alb_controller_version" {
+  description = "Version of AWS Load Balancer Controller Helm chart"
+  type        = string
+  default     = "1.7.1"
+}
+
+variable "enable_alb_controller" {
+  description = "Enable AWS Load Balancer Controller"
+  type        = bool
+  default     = true
 }
