63119e78dcc2a52acc00bdd7236c7337ba3f08ed
Chaithanya Gudiwada <116893885+chaigudi@users.noreply.github.com>
2025-06-03 17:04:25 +0530

    Update outputs.tf

--- code/meme-generator/terraform/outputs.tf
+++ code/meme-generator/terraform/outputs.tf
@@ -1,7 +1,132 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
+# VPC Outputs
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "nat_gateway_ids" {
+  description = "List of IDs of the NAT Gateways"
+  value       = module.vpc.natgw_ids
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = module.vpc.igw_id
+}
+
+# EKS Cluster Outputs
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_version" {
+  description = "The Kubernetes version for the EKS cluster"
+  value       = module.eks.cluster_version
+}
+
+output "cluster_platform_version" {
+  description = "Platform version for the EKS cluster"
+  value       = module.eks.cluster_platform_version
+}
+
+output "cluster_status" {
+  description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
+  value       = module.eks.cluster_status
+}
+
+output "cluster_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_security_group_arn" {
+  description = "Amazon Resource Name (ARN) of the cluster security group"
+  value       = module.eks.cluster_security_group_arn
+}
+
+# EKS Node Groups Outputs
+output "eks_managed_node_groups" {
+  description = "Map of attribute maps for all EKS managed node groups created"
+  value       = module.eks.eks_managed_node_groups
+}
+
+output "eks_managed_node_groups_autoscaling_group_names" {
+  description = "List of the autoscaling group names created by EKS managed node groups"
+  value       = module.eks.eks_managed_node_groups_autoscaling_group_names
+}
+
+# OIDC Provider Outputs
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.eks.cluster_oidc_issuer_url
+}
+
+output "oidc_provider_arn" {
+  description = "The ARN of the OIDC Provider if enabled"
+  value       = module.eks.oidc_provider_arn
+}
+
+# ALB Controller Outputs
+output "aws_load_balancer_controller_role_arn" {
+  description = "ARN of the AWS Load Balancer Controller IAM role"
+  value       = module.aws_load_balancer_controller_irsa_role.iam_role_arn
+}
+
+output "aws_load_balancer_controller_role_name" {
+  description = "Name of the AWS Load Balancer Controller IAM role"
+  value       = module.aws_load_balancer_controller_irsa_role.iam_role_name
+}
+
+# kubectl Configuration Command
+output "kubectl_config_command" {
+  description = "kubectl config command to configure your local environment"
+  value       = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+# Cluster Access Commands
+output "cluster_access_commands" {
+  description = "Commands to access the EKS cluster"
+  value = {
+    kubectl_config = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
+    cluster_info   = "kubectl cluster-info"
+    get_nodes      = "kubectl get nodes"
+    get_pods       = "kubectl get pods -A"
+  }
+}
+
+# Resource Tags
+output "common_tags" {
+  description = "Common tags applied to resources"
+  value       = var.common_tags
 }
